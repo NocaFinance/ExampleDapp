faff927d1b2f297e45e45d88ba6be87746b11567
Alexander Ludwig <alex@Alexanders-MacBook-Pro-3.local>
2022-11-04 23:15:38 -0700

    added first solidity contracts and hyperlane setup

---
+++ contracts/Communicator.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import {UserDeposit} from "./UserDeposit.sol";
+import {ProtocolConnector} from "./ProtocolConnector.sol";
+import {console2} from "lib/forge-std/src/console2.sol";
+
+interface IOutbox {
+    function dispatch(
+        uint32 _destinationDomain,
+        bytes32 _recipientAddress,
+        bytes calldata _messageBody
+    ) external returns (uint256);
+}
+
+interface IMessageRecipient {
+    function handle(
+        uint32 _origin,
+        bytes32 _sender,
+        bytes calldata _messageBody
+    ) external;
+}
+
+contract Communicator is IMessageRecipient {
+    uint16 chainId;
+    uint16 masterChainId;
+    address linkedAddress;
+    address hypOutbox;
+    uint32[] hypDomainIdentifier;
+    address[] public dstCommunicators;
+
+    function initialize(
+        uint16 _chainId,
+        uint16 _masterChainId,
+        address _linkedAddress,
+        address _hypOutbox,
+        uint32[] memory _hypDomainIdentifier,
+        address[] memory _dstCommunicators
+    ) public {
+        console2.log(_chainId);
+        console2.log(_masterChainId);
+        // console2.log(_satelliteAddress);
+
+        chainId = _chainId;
+        masterChainId = _masterChainId;
+        hypOutbox = _hypOutbox;
+        hypDomainIdentifier = _hypDomainIdentifier;
+        dstCommunicators = _dstCommunicators;
+
+        linkedAddress = _linkedAddress;
+    }
+
+    // alignment preserving cast
+    function _addressToBytes32(address _addr) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
+    }
+
+    function depositFunds(address user, uint256 amount) public {
+        _sendHyperlane(user, amount, "deposit", 1); // we always send to the chain deployed on 1
+    }
+
+    function depositFunds() public {
+        ProtocolConnector(linkedAddress).depositFunds(msg.sender, 10);
+    }
+
+    function withdrawFunds(address user, uint256 amount) public {
+        _sendHyperlane(user, amount, "withdraw", 1);
+    }
+
+    function releaseFunds(
+        address user,
+        uint256 amount,
+        uint16 originChainId
+    ) public {
+        _sendHyperlane(user, amount, "release", originChainId);
+    }
+
+    function _sendHyperlane(
+        address sender,
+        uint256 amount,
+        string memory fct,
+        uint16 _dstChainId
+    ) internal {}
+
+    function handle(
+        uint32 _origin,
+        bytes32 _sender,
+        bytes calldata _messageBody
+    ) external {}
+}
---
+++ contracts/ProtocolConector.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "lib/forge-std/src/Test.sol";
+import {console2} from "lib/forge-std/src/console2.sol";
+
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Communicator} from "../contracts/Communicator.sol";
+
+import {noERC20} from "./tokens/noERC20.sol";
+import {InoERC20} from "./tokens/InoERC20.sol";
+
+contract ProtocolConnector {
+    mapping(address => uint256) public usersId;
+    mapping(uint256 => mapping(address => uint256)) public investedAPWine;
+    uint256 public countUser;
+
+    InoERC20 goldfinch;
+
+    Communicator communicator;
+
+    function initialize(address _goldfinch, address _communicator) public {
+        goldfinch = InoERC20(_goldfinch);
+        communicator = Communicator(_communicator);
+    }
+
+    modifier onlyCommunicator() {
+        require(
+            msg.sender == address(communicator),
+            "Only communicator allowed."
+        );
+        _;
+    }
+
+    // deposits funds into the goldfinch protocol
+    function depositFunds(address sender, uint256 amount) public {
+        //onlyCommunicator
+        atlendis.mint(sender, amount);
+    }
+
+    function withdrawFunds(
+        address sender,
+        uint256 amount,
+        uint16 originChainId
+    ) public onlyCommunicator {
+        require(
+            goldfinch.balanceOf(sender) >= amount,
+            "User does not have enough funds."
+        );
+
+        atlendis.burn(sender, amount);
+        communicator.releaseFunds(sender, amount, originChainId);
+    }
+}
