761bac9f275b821f16b28fc021205019fffb634f
Alexander Ludwig <alex@Alexanders-MacBook-Pro-3.local>
2022-11-05 21:26:40 -0700

    fixed batching

--- script/checkCrossChain.sh
+++ script/checkCrossChain.sh
@@ -1,14 +1,16 @@
 
 source .env
 
-export contractSatellite="0x53a1301f3649949405672021679b1279ad763655"
-export contractCore="0xaaf4b546ea698e795bd860a502fb27eb2fb7cac9" 
-
+export contractDeposit="0xcab85be2c1d684b64a760e336d4c06b216461ade"
 
 #fund wallet
-cast send --rpc-url $GOERLI_RPC_URL --private-key $PRIVATE_KEY  --value 100000000000 $contractSatellite
+#cast send --rpc-url $GOERLI_RPC_URL --private-key $PRIVATE_KEY  --value 100000000000 $contractSatellite
 
 #initiate cross chain comm
-#cast send --rpc-url $MOONBASE_RPC_URL --private-key $PRIVATE_KEY $contractCore "initUserAccount()"
-#cast send --rpc-url $MOONBASE_RPC_URL --private-key $PRIVATE_KEY $contractCore "investAPWineETH(uint256)" 50
+#cast send --rpc-url $FUJI_RPC_URL --private-key $PRIVATE_KEY $contractDeposit "initAccount()"
+#cast send --rpc-url $FUJI_RPC_URL --private-key $PRIVATE_KEY $contractDeposit "deposit(uint256)" 500
+cast send --rpc-url $FUJI_RPC_URL --private-key $PRIVATE_KEY $contractDeposit "deposit(uint256)" 500
+cast send --rpc-url $FUJI_RPC_URL --private-key $PRIVATE_KEY $contractDeposit "sendBatched()"
+
+
 
